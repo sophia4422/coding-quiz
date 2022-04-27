814ccb79764ea1d080ccb2db72e37796ff0833c8
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-04-27 21:13:44 +0100

    pseudo code

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,60 @@
-console.log("hey");
+//the penalty is -5 seconds
+
+if () {}
+// if the counter is === a minus number then render game over page
+
+
+//global declarations
+const questions = [];
+let timerValue = 10 * questions.length;
+
+const onLoad = () => {};
+// initialise local storage
+// check if highscores exists in LS
+// if false then set highscores to empty array in LS
+
+const removeStartSection = () => {};
+
+const startTimer = () => {};
+//declare function to execute every 1 sec
+//read 0 to stop game
+const countdown = () => {
+  //decrement timer value
+  //check if timer reachers 0
+  //if true render game over
+};
+//setInterval to 1000ms (1s)
+
+const validateAnswer = () => {
+  //get answer clicked from user
+  // get the correct answer for question
+  // compare the 2 answers
+  // if incorrect subtract 5 seconds from timerValue
+  // if incorrect render error alert with message and status
+  // Go to next question
+  // if correct render success alert with message and status
+  // go to next question
+};
+
+const renderTimerSection = () => {};
+// use html as guide and build in JS
+//append section to main
+
+const renderQuestionSection = () => {};
+// use html as guide and build in JS
+//append section to main
+// add click event listener on question-section
+
+const renderGameOver = () => {};
+// use html as guide and build in JS
+//append section to main
+
+const renderAlert = (message, statu) => {};
+//append to main div to #question-section
+
+const startQuiz = () => {};
+//remove start section
+
+//start timer
+
+//render time section
