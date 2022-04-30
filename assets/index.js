763d5ce438a474c92b580c0f84f78f02e6890f5c
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-04-30 21:17:46 +0100

    everything working checking with console logs

--- assets/index.js
+++ assets/index.js
@@ -3,23 +3,61 @@
 // I click 'Start Quiz'
 const startButton = document.getElementById("start-btn");
 
+//target the general knowledge / start section to remove it
+const startSection = document.getElementById("start-quiz-banner");
+
+//function to render question on page
+const renderQuestion = () => {
+  console.log("render question 1");
+};
+
+//function to remove general know / start section on page
+const removeStartSection = () => {
+  console.log("remove start section");
+};
+
 const startQuiz = () => {
-  // The 'General Knowledge' Page disappears
-  // The first question, the timer and the answers appear
+  console.log("start button clicked");
+  // The 'General Knowledge' / start section Page disappears
+  removeStartSection();
+  // Render first question
+  renderQuestion();
+  //the timer and the answers
+
   // The timer starts to count down
-  console.log("HEY");
 };
 
 startButton.addEventListener("click", startQuiz);
 
-// I click an answer
-// If the answer is correct, the next question loads
-// If the answer is wrong, the next question loads and -10 seconds is deducted from the timer
+// const renderTimerSection = () => {};
+// // use html as guide and build in JS
+// //append section to main
 
-// Once I have finished all the questions
-// Or the timer runs out
-// Then the form page loads
-// I enter my name and my score is displayed
+// const renderQuestionSection = () => {};
+// // use html as guide and build in JS
+// //append section to main
+// // add click event listener on question-section
 
-//If I click home, the 'General Knowledge Quiz' page loads
-// If I click 'High Scores' my names / scores are pulled from LS and displayed (???)
+// const startTimer = () => {
+//   console.log("started");
+// };
+// //declare function to execute every 1 sec
+// //read 0 to stop game
+// const countdown = () => {
+//   //decrement timer value
+//   //check if timer reachers 0
+//   //if true render game over
+// };
+// //setInterval to 1000ms (1s)
+
+// // I click an answer
+// // If the answer is correct, the next question loads
+// // If the answer is wrong, the next question loads and -10 seconds is deducted from the timer
+
+// // Once I have finished all the questions
+// // Or the timer runs out
+// // Then the form page loads
+// // I enter my name and my score is displayed
+
+// //If I click home, the 'General Knowledge Quiz' page loads
+// // If I click 'High Scores' my names / scores are pulled from LS and displayed (???)
