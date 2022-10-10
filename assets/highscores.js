b68af2577c49dfaa7221b34d3b9e030ef12ce0ac
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-10-10 19:06:01 +0100

    highscores table renders

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,6 +1,6 @@
 const mainElement = document.getElementById("main");
 
-// const highScoresSection = document.getElementById("no-highscores");
+const highScoresSection = document.getElementById("no-highscores");
 
 const readFromLocalStorage = () => {
   // get from LS by key and parse
@@ -36,27 +36,67 @@ const displayScores = () => {
     hsSection.append(playBtn);
     mainElement.append(hsSection);
   } else {
-    const tableHeader = document.getElementById("all-scores");
+    const tableSection = document.createElement("section");
+    tableSection.setAttribute("class", "section-container");
+    tableSection.setAttribute("id", "highscores-section");
+
+    const titleTwo = document.createElement("h1");
+    titleTwo.setAttribute("class", "highscore-title");
+    titleTwo.textContent = "Highscores";
+
+    const tableHeader = document.createElement("table");
+    tableHeader.setAttribute("id", "all-scores");
 
     highScores.forEach((highScore) => {
       const tableRow = document.createElement("tr");
 
-      const tableName = document.createElement("td");
-      tableName.textContent = highScore.userName;
-
       const tableScore = document.createElement("td");
       tableScore.textContent = highScore.score;
+
+      const tableName = document.createElement("td");
+      tableName.textContent = highScore.userName;
 
       tableHeader.append(tableRow);
 
       tableRow.append(tableName, tableScore);
     });
+    tableSection.append(titleTwo);
+    tableSection.append(tableHeader);
+
+    mainElement.append(tableSection);
   }
 };
+window.addEventListener("load", displayScores);
+
+// const div = document.createElement("div");
+// div.setAttribute("class", "highscores-section");
+
+// const tableHeader = document.getElementById("all-scores");
+
+// highScores.forEach((highScore) => {
+//   const tableRow = document.createElement("tr");
+
+//   const tableName = document.createElement("td");
+//   tableName.textContent = "hiiii"
+
+//   highScore.userName;
+
+//   const tableScore = document.createElement("td");
+//   tableScore.textContent = highScore.score;
+
+//   tableSection.append(div, tableHeader);
+
+//   tableHeader.append(tableRow);
+
+//   tableRow.append(tableName, tableScore);
+
+//   tableRow.append(tableName);
+//   tableHeader.append(tableRow);
+//   div.append(tableHeader);
+//   tableSection.append(div);
+//   mainElement.append(tableSection);
 
 // document.getElementById("clear-scores").addEventListener("click", () => {
 //   localStorage.clear();
 //   displayScores();
 // });
-
-window.addEventListener("load", displayScores);
